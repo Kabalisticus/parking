28138ef2322be305760bf785bb27d7ada2eaf289
Mateusz Malinowski <kabalisticus@gmail.com>
2023-10-06 11:50:17 +0200

    models file missing in previous update

--- models.py
+++ models.py
@@ -1,11 +1,15 @@
 from typing import Optional
-from pydantic import BaseModel, constr, condecimal
+from pydantic import BaseModel, validator, constr, condecimal
 from uuid import UUID
 from enum import Enum
 from datetime import date
 from fastapi import FastAPI
 
+from starlette.exceptions import HTTPException 
+from starlette.status import HTTP_503_SERVICE_UNAVAILABLE, HTTP_400_BAD_REQUEST
+
 app = FastAPI()
+
 
 class PlateNumber(BaseModel):
     """
@@ -33,11 +37,23 @@ class SubscriptionRequest(PlateNumber):
 
     Attributes:
         plate_number (str): The plate number in the format 'XX-XXXX' or 'XXX-XXXXX'.
-        date_start (date): The start date of the subscription.
-        date_end (date): The end date of the subscription.
+        start_date (date): The start date of the subscription.
+        end_date (date): The end date of the subscription.
     """
-    date_start: date  
-    date_end: date
+    start_date: date  
+    end_date: date
+
+    @validator("end_date", pre=False, always=True)
+    def validate_dates(cls,end_date,values):
+        start_date = values.get("start_date")
+
+        if end_date <= start_date:
+            raise HTTPException(
+                status_code=HTTP_400_BAD_REQUEST,            
+                detail="End date cannot be earlier than start date"
+                )
+        return end_date
+
 
 class ExitRequest (PlateNumber):
     """
@@ -49,6 +65,16 @@ class ExitRequest (PlateNumber):
     """
     exit_date: date
 
+class EntryRequest (PlateNumber):
+    """
+    Pydantic model for an entry request.
+
+    Attributes:
+        plate_number (str): The plate number in the format 'XX-XXXX' or 'XXX-XXXXX'.
+        date_entry (date): The date of entry to the parking.
+    """
+    date_entry: date
+
 class PaymentRequest (BaseModel):
     """
     Pydantic model for a payment request.
@@ -59,67 +85,94 @@ class PaymentRequest (BaseModel):
         date_payment (date): The date of the payment.
     """
     ticket_number: int
-    payment_value: condecimal(max_digits=10, decimal_places=2)
+    payment_value: condecimal(max_digits=4, decimal_places=2)
     date_payment: date 
 
-class EntryRequest (BaseModel):
+class EarningsRequest(BaseModel):
     """
-    Pydantic model for an entry request.
+    Pydantic model for a earnings request.
 
     Attributes:
-        plate_number (str): The plate number in the format 'XX-XXXX' or 'XXX-XXXXX'.
-        date_entry (date): The date of entry to the parking.
+        start_date (date): The start date for earnings check.
+        end_date (date): The end date for earnings check.
     """
-    plate_number: constr(regex=r'^[A-Z]{2,3}-[A-Z0-9]{4,5}$')
-    date_entry: date
-    plate_number: constr(regex=r'^[A-Z]{2,3}-[A-Z0-9]{4,5}$')
-    date_entry: date
+    start_date: date  
+    end_date: date
 
-class Subscription(BaseModel):
+    @validator("end_date", pre=False, always=True)
+    def validate_dates(cls,end_date,values):
+        start_date = values.get("start_date")
+
+        if end_date <= start_date:
+            raise HTTPException(
+                status_code=HTTP_400_BAD_REQUEST,            
+                detail="End date cannot be earlier than start date"
+                )
+        return end_date
+
+class Subscription(PlateNumber):
     """
     Pydantic model for a subscription.
 
     Attributes:
-        subscription_ID (int): The ID of the subscription.
         plate_number (str): The plate number in the format 'XX-XXXX' or 'XXX-XXXXX'.
+        subscription_ID (int): The ID of the subscription.
         start_date (date): The start date of the subscription.
         end_date (date): The end date of the subscription.
     """
     subscription_ID: int
-    plate_number: constr(regex=r'^[A-Z]{2,3}-[A-Z0-9]{4,5}$')
     start_date: date
     end_date: date
 
-class Vehicle(BaseModel):
+    @validator("end_date", pre=False, always=True)
+    def validate_dates(cls,end_date,values):
+        start_date = values.get("start_date")
+
+        if end_date <= start_date:
+            raise HTTPException(
+                status_code=HTTP_400_BAD_REQUEST,            
+                detail="End date cannot be earlier than start date"
+                )
+        return end_date
+
+class Vehicle(PlateNumber):
     """
     Pydantic model for a vehicle.
 
     Attributes:
-        vehicle_ID (UUID): The unique identifier for the vehicle.
         plate_number (str): The plate number in the format 'XX-XXXX' or 'XXX-XXXXX'.
+        vehicle_ID (UUID): The unique identifier for the vehicle.
     """
     vehicle_ID: UUID
-    plate_number: constr(regex=r'^[A-Z]{2,3}-[A-Z0-9]{4,5}$')
 
-
-class EntryExitRegister(BaseModel):
+class EntryExitRegister(PlateNumber):
     """
     Pydantic model for an entry/exit registration.
 
     Attributes:
-        ticket_ID (UUID): The unique identifier for the ticket.
         plate_number (str): The plate number in the format 'XX-XXXX' or 'XXX-XXXXX'.
+        ticket_ID (UUID): The unique identifier for the ticket.
         entry_time (date): The date and time of entry.
         exit_time (Optional[date]): The date and time of exit (optional).
         amount (str): The payment amount in the format 'X.XX'.
         payment_status (PaymentStatus): The status of payment.
     """
     ticket_ID: UUID
-    plate_number: constr(regex=r'^[A-Z]{2,3}-[A-Z0-9]{4,5}$')
     entry_time: date 
     exit_time: Optional [date]
-    amount: condecimal(max_digits=10, decimal_places=2)
+    amount: condecimal(max_digits=4, decimal_places=2)
     payment_status: PaymentStatus
+
+    @validator("exit_time", pre=False, always=True)
+    def validate_dates(cls,exit_time,values):
+        entry_time = values.get("entry_time")
+
+        if exit_time <= entry_time:
+            raise HTTPException(
+                status_code=HTTP_400_BAD_REQUEST,            
+                detail="End date cannot be earlier than start date"
+                )
+        return exit_time
 
 class Payments(BaseModel):
     """
@@ -133,5 +186,5 @@ class Payments(BaseModel):
     """
     payment_ID: UUID
     ticket_number: int
-    amount: condecimal(max_digits=10, decimal_places=2)
+    amount: condecimal(max_digits=4, decimal_places=2)
     payment_status: PaymentStatus
